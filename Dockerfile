d8c6b8cf5938a30d0afa79b01ec6e7a76962ac15
xia1997x <nikolamihov2@gmail.com>
2025-04-16 13:31:16 +0200

    init commit add python application

---
+++ Dockerfile
@@ -0,0 +1,40 @@
+# syntax=docker/dockerfile:1
+ARG PYTHON_VERSION=3.13.2-alpine3.21
+FROM python:${PYTHON_VERSION} AS base
+
+# Prevents Python from writing pyc files.
+ENV PYTHONDONTWRITEBYTECODE=1
+
+# Keeps Python from buffering stdout and stderr to avoid situations where
+# the application crashes without emitting any logs due to buffering.
+ENV PYTHONUNBUFFERED=1
+
+WORKDIR /app
+
+# Create a non-privileged user that the app will run under.
+# See https://docs.docker.com/go/dockerfile-user-best-practices/
+ARG UID=10001
+RUN adduser \
+    --disabled-password \
+    --gecos "" \
+    --home "/nonexistent" \
+    --shell "/sbin/nologin" \
+    --no-create-home \
+    --uid "${UID}" \
+    appuser
+
+RUN chown -R appuser /app
+RUN chmod -R u+rwx /app
+
+RUN --mount=type=cache,target=/root/.cache/pip \
+    --mount=type=bind,source=requirements.txt,target=requirements.txt \
+    python -m pip install -r requirements.txt
+
+# Switch to the non-privileged user to run the application.
+USER appuser
+
+# Copy the source code into the container.
+COPY . .
+
+# Run the application.
+CMD ["fastapi", "run", "main.py"]
---
+++ main.py
@@ -0,0 +1,75 @@
+import socket
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get("/")
+async def get_hostname():
+    """Returns the hostname of the server with some ugly css animations for fun"""
+    html_content = f"""
+    <html>
+    <head>
+        <style>
+            @keyframes rainbow-text {{
+                0% {{ color: red; transform: scale(0); }}
+                6.67% {{ color: orangered; transform: scale(1.5); }}
+                13.34% {{ color: orange; transform: scale(1); }}
+                20.01% {{ color: gold; transform: scale(1.5); }}
+                26.68% {{ color: yellow; transform: scale(1); }}
+                33.35% {{ color: lime; transform: scale(1.5); }}
+                40.02% {{ color: green; transform: scale(1); }}
+                46.69% {{ color: teal; transform: scale(1.5); }}
+                53.36% {{ color: cyan; transform: scale(1); }}
+                60.03% {{ color: dodgerblue; transform: scale(1.5); }}
+                66.70% {{ color: blue; transform: scale(1); }}
+                73.37% {{ color: indigo; transform: scale(1.5); }}
+                80.04% {{ color: violet; transform: scale(1); }}
+                86.71% {{ color: pink; transform: scale(1.5); }}
+                93.38% {{ color: deeppink; transform: scale(1); }}
+                100% {{ color: red; transform: scale(1.5); }}
+            }}
+
+            @keyframes rainbow-bg {{
+                0% {{ background-color: blue; }}
+                6.67% {{ background-color: indigo; }}
+                13.34% {{ background-color: violet; }}
+                20.01% {{ background-color: pink; }}
+                26.68% {{ background-color: deeppink; }}
+                33.35% {{ background-color: red; }}
+                40.02% {{ background-color: orangered; }}
+                46.69% {{ background-color: orange; }}
+                53.36% {{ background-color: gold; }}
+                60.03% {{ background-color: yellow; }}
+                66.70% {{ background-color: lime; }}
+                73.37% {{ background-color: green; }}
+                80.04% {{ background-color: teal; }}
+                86.71% {{ background-color: cyan; }}
+                93.38% {{ background-color: dodgerblue; }}
+                100% {{ background-color: blue; }} /* Loops back */
+            }}
+
+            body {{
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                height: 100vh;
+                margin: 0;
+                font-family: comic sans ms;
+                animation: rainbow-bg 20s infinite ease-in-out reverse;
+            }}
+
+            h1 {{
+                text-align: center;
+                font-size: 120px;
+                font-weight: bold;
+                animation: rainbow-text 5s infinite ease-in-out;
+            }}
+        </style>
+    </head>
+    <body>
+        <h1>{socket.gethostname()}</h1>
+    </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content, status_code=418)
